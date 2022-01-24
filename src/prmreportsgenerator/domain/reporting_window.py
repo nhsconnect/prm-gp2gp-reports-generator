a0902f4e789c2fc625b0ad7f50cb11dabd1ca061
Serge Pogadaev <serge.pogadaev1@nhs.net>
2022-01-24 14:13:07 +0000

    PRMT-2516 [SP] Add validation to report window for number of days and number of months input

--- src/prmreportsgenerator/domain/reporting_window.py
+++ src/prmreportsgenerator/domain/reporting_window.py
@@ -1,5 +1,10 @@
+from typing import Optional
+
+
 class ReportingWindow:
-    def __init__(self, number_of_months: int, number_of_days: int):
+    def __init__(self, number_of_months: Optional[int], number_of_days: Optional[int]):
 
         if number_of_months and number_of_days:
             raise ValueError("Cannot have both number of months and number of days")
+        if number_of_months is None and number_of_days is None:
+            raise ValueError("Number of months or number of days must be specified")
--- tests/unit/domain/test_reporting_window.py
+++ tests/unit/domain/test_reporting_window.py
@@ -7,3 +7,9 @@ def test_throws_value_error_given_both_number_of_days_and_months():
     with pytest.raises(ValueError) as e:
         ReportingWindow(number_of_months=1, number_of_days=1)
     assert str(e.value) == "Cannot have both number of months and number of days"
+
+
+def test_throws_value_error_given_no_number_of_days_and_months():
+    with pytest.raises(ValueError) as e:
+        ReportingWindow(number_of_months=None, number_of_days=None)
+    assert str(e.value) == "Number of months or number of days must be specified"
