7291c2dcae75ffd652b10566ed4158e1b9c070aa
Serge Pogadaev <serge.pogadaev1@nhs.net>
2022-01-24 14:25:36 +0000

    PRMT-2516 [SP] Add start datetime to report window

--- src/prmreportsgenerator/domain/reporting_window.py
+++ src/prmreportsgenerator/domain/reporting_window.py
@@ -1,10 +1,23 @@
+from datetime import datetime, time
 from typing import Optional
 
 
 class ReportingWindow:
-    def __init__(self, number_of_months: Optional[int], number_of_days: Optional[int]):
+    def __init__(
+        self,
+        start_datetime: Optional[datetime],
+        number_of_months: Optional[int],
+        number_of_days: Optional[int],
+    ):
+        self._validate_datetime_is_at_midnight(start_datetime)
 
         if number_of_months and number_of_days:
             raise ValueError("Cannot have both number of months and number of days")
         if number_of_months is None and number_of_days is None:
             raise ValueError("Number of months or number of days must be specified")
+
+    @staticmethod
+    def _validate_datetime_is_at_midnight(a_datetime: Optional[datetime]):
+        midnight = time(hour=0, minute=0, second=0)
+        if a_datetime and a_datetime.time() != midnight:
+            raise ValueError("Datetime must be at midnight")
--- tests/unit/domain/test_reporting_window.py
+++ tests/unit/domain/test_reporting_window.py
@@ -1,15 +1,26 @@
+from datetime import datetime
+
 import pytest
+from dateutil.tz import UTC
 
 from prmreportsgenerator.domain.reporting_window import ReportingWindow
 
 
 def test_throws_value_error_given_both_number_of_days_and_months():
     with pytest.raises(ValueError) as e:
-        ReportingWindow(number_of_months=1, number_of_days=1)
+        ReportingWindow(start_datetime=None, number_of_months=1, number_of_days=1)
     assert str(e.value) == "Cannot have both number of months and number of days"
 
 
 def test_throws_value_error_given_no_number_of_days_and_months():
     with pytest.raises(ValueError) as e:
-        ReportingWindow(number_of_months=None, number_of_days=None)
+        ReportingWindow(start_datetime=None, number_of_months=None, number_of_days=None)
     assert str(e.value) == "Number of months or number of days must be specified"
+
+
+def test_throws_value_error_given_start_datetime_not_at_midnight():
+    start_datetime = datetime(year=2019, month=12, day=1, hour=5, minute=0, second=0, tzinfo=UTC)
+
+    with pytest.raises(ValueError) as e:
+        ReportingWindow(start_datetime=start_datetime, number_of_months=1, number_of_days=None)
+    assert str(e.value) == "Datetime must be at midnight"
