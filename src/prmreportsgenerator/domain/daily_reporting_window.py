8d3ab709dbb708c5c0f6618b1df53571ee0be408
helen.zhou <helen.zhou1@nhs.net>
2022-01-25 17:17:25 +0000

    PRMT-2516 [HZ] Add dates property to daily reporting window

--- src/prmreportsgenerator/domain/daily_reporting_window.py
+++ src/prmreportsgenerator/domain/daily_reporting_window.py
@@ -1,7 +1,9 @@
 from datetime import datetime, time, timedelta
-from typing import Optional
+from typing import List
 
 from dateutil.tz import UTC
+
+from prmreportsgenerator.utils.date_converter import convert_date_range_to_dates
 
 
 class DailyReportingWindow:
@@ -16,6 +18,11 @@ class DailyReportingWindow:
         return today_midnight_utc
 
     @property
-    def start_datetime(self) -> Optional[datetime]:
+    def start_datetime(self) -> datetime:
         today_midnight_datetime = self._calculate_today_midnight_datetime()
         return today_midnight_datetime - timedelta(days=self._number_of_days + self._cutoff_days)
+
+    @property
+    def dates(self) -> List[datetime]:
+        end_datetime = self.start_datetime + timedelta(days=self._number_of_days)
+        return convert_date_range_to_dates(self.start_datetime, end_datetime)
--- tests/unit/domain/test_daily_reporting_window.py
+++ tests/unit/domain/test_daily_reporting_window.py
@@ -5,6 +5,7 @@ from dateutil.tz import UTC
 from freezegun import freeze_time
 
 from prmreportsgenerator.domain.daily_reporting_window import DailyReportingWindow
+from tests.builders.common import a_datetime
 
 
 @pytest.mark.parametrize(
@@ -41,3 +42,38 @@ def test_returns_today_midnight_minus_days_given_1_number_of_days_and_various_cu
     actual = reporting_window.start_datetime
 
     assert actual == expected_start_datetime
+
+
+@freeze_time(a_datetime(year=2022, month=1, day=3))
+def test_dates_property_returns_list_of_datetimes_given_4_number_of_days_and_0_cutoff():
+    reporting_window = DailyReportingWindow(number_of_days=4, cutoff_days=0)
+
+    expected = [
+        datetime(year=2021, month=12, day=30, tzinfo=UTC),
+        datetime(year=2021, month=12, day=31, tzinfo=UTC),
+        datetime(year=2022, month=1, day=1, tzinfo=UTC),
+        datetime(year=2022, month=1, day=2, tzinfo=UTC),
+    ]
+
+    actual = reporting_window.dates
+
+    assert actual == expected
+
+
+@pytest.mark.parametrize(
+    "cutoff_days, expected_dates",
+    [
+        (1, [datetime(year=2021, month=12, day=30, hour=0, minute=0, second=0, tzinfo=UTC)]),
+        (3, [datetime(year=2021, month=12, day=28, hour=0, minute=0, second=0, tzinfo=UTC)]),
+        (10, [datetime(year=2021, month=12, day=21, hour=0, minute=0, second=0, tzinfo=UTC)]),
+    ],
+)
+@freeze_time(a_datetime(year=2022, month=1, day=1))
+def test_dates_property_returns_list_of_datetimes_given_1_day_and_various_cutoffs(
+    cutoff_days, expected_dates
+):
+    reporting_window = DailyReportingWindow(cutoff_days=cutoff_days, number_of_days=1)
+
+    actual = reporting_window.dates
+
+    assert actual == expected_dates
