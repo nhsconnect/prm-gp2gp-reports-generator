8c9e13c96d2eed17b6e268974203a0484a4993d1
Jake Newman <jacob.newman1@nhs.net>
2022-01-27 16:07:32 +0000

    PRMT-2516 [HZ/JN] Use ReportingWindowCalculator to generate DailyReportingWindow with number_of_days and cutoff_days

--- src/prmreportsgenerator/domain/reporting_windows/reporting_window_calculator.py
+++ src/prmreportsgenerator/domain/reporting_windows/reporting_window_calculator.py
@@ -5,6 +5,7 @@ from typing import Optional
 from prmreportsgenerator.domain.reporting_windows.custom_reporting_window import (
     CustomReportingWindow,
 )
+from prmreportsgenerator.domain.reporting_windows.daily_reporting_window import DailyReportingWindow
 from prmreportsgenerator.domain.reporting_windows.reporting_window import ReportingWindow
 
 logger = logging.getLogger(__name__)
@@ -13,9 +14,14 @@ logger = logging.getLogger(__name__)
 class ReportingWindowCalculator:
     @classmethod
     def generate(
-        cls, start_datetime: Optional[datetime], end_datetime: Optional[datetime]
+        cls,
+        start_datetime: Optional[datetime] = None,
+        end_datetime: Optional[datetime] = None,
+        number_of_days: Optional[int] = None,
+        cutoff_days: Optional[int] = None,
     ) -> ReportingWindow:
         if start_datetime and end_datetime:
             return CustomReportingWindow(start_datetime, end_datetime)
-
+        if number_of_days and cutoff_days:
+            return DailyReportingWindow(number_of_days, cutoff_days)
         raise ValueError("Missing required config to generate reports")
--- tests/unit/domain/reporting_windows/test_reporting_window_generator.py
+++ tests/unit/domain/reporting_windows/test_reporting_window_generator.py
@@ -6,24 +6,30 @@ from dateutil.tz import UTC
 from prmreportsgenerator.domain.reporting_windows.custom_reporting_window import (
     CustomReportingWindow,
 )
+from prmreportsgenerator.domain.reporting_windows.daily_reporting_window import DailyReportingWindow
 from prmreportsgenerator.domain.reporting_windows.reporting_window_calculator import (
     ReportingWindowCalculator,
 )
 from tests.builders.common import a_datetime
 
 
-def test_generates_daily_reporting_window_given_start_datetime_and_end_datetime():
-    start_datetime = datetime(year=2021, month=12, day=30, tzinfo=UTC)
-    end_datetime = datetime(year=2022, month=1, day=3, tzinfo=UTC)
+def test_generates_custom_reporting_window_given_start_datetime_and_end_datetime():
+    a_start_datetime = datetime(year=2021, month=12, day=30, tzinfo=UTC)
+    a_end_datetime = datetime(year=2022, month=1, day=3, tzinfo=UTC)
 
-    reporting_window = ReportingWindowCalculator.generate(start_datetime, end_datetime)
+    reporting_window = ReportingWindowCalculator.generate(
+        start_datetime=a_start_datetime, end_datetime=a_end_datetime
+    )
 
-    actual = reporting_window
+    assert isinstance(reporting_window, CustomReportingWindow)
+    assert len(reporting_window.get_dates()) == 4
 
-    assert isinstance(actual, CustomReportingWindow)
-    assert actual.start_datetime == start_datetime
-    assert actual.end_datetime == end_datetime
-    assert len(actual.get_dates()) == 4
+
+def test_generates_daily_reporting_window_given_number_of_days_and_cutoff_days():
+    reporting_window = ReportingWindowCalculator.generate(number_of_days=2, cutoff_days=1)
+
+    assert isinstance(reporting_window, DailyReportingWindow)
+    assert len(reporting_window.get_dates()) == 2
 
 
 @pytest.mark.parametrize(
