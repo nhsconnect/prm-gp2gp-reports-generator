e7e59e1551a37d7ff3536881e7fa4967d276012f
helen.zhou <helen.zhou1@nhs.net>
2022-02-01 10:44:22 +0000

    PRMT-2516 [HZ/JN] Refactor calculating start datetime in daily reporting window

--- src/prmreportsgenerator/domain/reporting_windows/daily_reporting_window.py
+++ src/prmreportsgenerator/domain/reporting_windows/daily_reporting_window.py
@@ -8,16 +8,19 @@ class DailyReportingWindow(ReportingWindow):
     def __init__(self, number_of_days: int, cutoff_days: int):
         self._number_of_days = number_of_days
         self._cutoff_days = cutoff_days
+        self._today_midnight_datetime = calculate_today_midnight_datetime()
+
         super().__init__(self.start_datetime, self.end_datetime)
 
     @property
     def start_datetime(self) -> datetime:
-        today_midnight_datetime = calculate_today_midnight_datetime()
-        return today_midnight_datetime - timedelta(days=self._number_of_days + self._cutoff_days)
+        return self._today_midnight_datetime - timedelta(
+            days=self._number_of_days + self._cutoff_days
+        )
 
     @property
     def end_datetime(self) -> datetime:
-        return self.start_datetime + timedelta(days=self._number_of_days)
+        return self._today_midnight_datetime - timedelta(days=self._cutoff_days)
 
     @property
     def config_string(self) -> str:
