75f25032555367657476201d2579ec385213004e
Serge Pogadaev <serge.pogadaev1@nhs.net>
2022-03-10 16:47:05 +0000

    PRMT-2543 [SP/JN] Add logic to CCGLevelIntegrationTimesReport to calculate transfers integrated within 3 days

--- src/prmreportsgenerator/domain/reports_generator/ccg_level_integration_times.py
+++ src/prmreportsgenerator/domain/reports_generator/ccg_level_integration_times.py
@@ -3,12 +3,39 @@ import pyarrow as pa
 from polars import DataFrame, col, count
 
 from prmreportsgenerator.domain.reports_generator.reports_generator import ReportsGenerator
+from prmreportsgenerator.domain.transfer import TransferStatus
+
+THREE_DAYS_IN_SECONDS = 259200
+EIGHT_DAYS_IN_SECONDS = 691200
+
+
+def assign_to_sla_band(sla_duration: int) -> str:
+    sla_duration_in_seconds = sla_duration
+    if sla_duration_in_seconds <= THREE_DAYS_IN_SECONDS:
+        return "WITHIN_3_DAYS"
+    elif sla_duration_in_seconds <= EIGHT_DAYS_IN_SECONDS:
+        return "WITHIN_8_DAYS"
+    else:
+        return "BEYOND_8_DAYS"
 
 
 class CCGLevelIntegrationTimesReportsGenerator(ReportsGenerator):
     def __init__(self, transfers: pa.Table):
         super().__init__()
         self._transfers = transfers
+
+    def _assign_sla_band(self, transfer_dataframe: DataFrame) -> DataFrame:
+        sla_band_column = col("sla_duration").apply(assign_to_sla_band).alias("sla_band")
+        return transfer_dataframe.with_column(sla_band_column)
+
+    def _calculate_integrated_within_3_days(self, transfer_dataframe: DataFrame) -> DataFrame:
+        integrated_within_3_days = (col("sla_band") == "WITHIN_3_DAYS") & (
+            col("status") == TransferStatus.INTEGRATED_ON_TIME.value
+        )
+        sum_of_integrated_within_3_days = integrated_within_3_days.sum()
+        return transfer_dataframe.with_column(
+            sum_of_integrated_within_3_days.alias("Integrated within 3 days")
+        )
 
     def _rename_columns(self, transfer_dataframe: DataFrame) -> DataFrame:
         return (
@@ -18,13 +45,27 @@ class CCGLevelIntegrationTimesReportsGenerator(ReportsGenerator):
                     col("requesting_practice_ccg_ods_code").alias("CCG ODS"),
                     col("requesting_practice_name").alias("Requesting practice name"),
                     col("requesting_practice_ods_code").alias("Requesting practice ODS"),
+                    col("Integrated within 3 days"),
                 ]
             )
-            .groupby(["Requesting practice name", "CCG name", "CCG ODS", "Requesting practice ODS"])
+            .groupby(
+                [
+                    "Requesting practice name",
+                    "CCG name",
+                    "CCG ODS",
+                    "Requesting practice ODS",
+                    "Integrated within 3 days",
+                ]
+            )
             .agg([count("conversation_id").alias("GP2GP Transfers received")])
         )
 
     def generate(self) -> pa.Table:
         transfers_frame = pl.from_arrow(self._transfers)
-        processed_transfers = self._process(transfers_frame, self._rename_columns).to_dict()
+        processed_transfers = self._process(
+            transfers_frame,
+            self._assign_sla_band,
+            self._calculate_integrated_within_3_days,
+            self._rename_columns,
+        ).to_dict()
         return pa.table(processed_transfers)
--- tests/unit/domain/test_generate_ccg_level_integration_times.py
+++ tests/unit/domain/test_generate_ccg_level_integration_times.py
@@ -1,3 +1,5 @@
+from datetime import timedelta
+
 import pyarrow as pa
 import pytest
 
@@ -10,13 +12,13 @@ from tests.builders.pa_table import PaTableBuilder
 
 
 @pytest.mark.filterwarnings("ignore:Conversion of")
-def test_returns_table_with_ccg_level_integration_times_columns():
+def test_returns_table_with_ccg_level_integration_times_columns_with_integrated_within_3_days():
     requesting_practice_name = a_string(6)
     requesting_practice_ods_code = a_string(6)
     requesting_practice_ccg_ods_code = a_string(3)
     requesting_practice_ccg_name = a_string(6)
     conversation_id = a_string(16)
-    status = TransferStatus.TECHNICAL_FAILURE.value
+    status = TransferStatus.INTEGRATED_ON_TIME.value
     failure_reason = TransferFailureReason.FINAL_ERROR.value
     table = (
         PaTableBuilder()
@@ -28,6 +30,7 @@ def test_returns_table_with_ccg_level_integration_times_columns():
             conversation_id=conversation_id,
             status=status,
             failure_reason=failure_reason,
+            sla_duration=timedelta(days=1).total_seconds(),
         )
         .build()
     )
@@ -41,7 +44,7 @@ def test_returns_table_with_ccg_level_integration_times_columns():
             "Requesting practice name",
             "Requesting practice ODS",
             "GP2GP Transfers received",
-            # "Integrated within 3 days",
+            "Integrated within 3 days",
             # "Integrated within 3 days - %",
             # "Integrated within 8 days",
             # "Integrated within 8 days - %",
@@ -61,7 +64,76 @@ def test_returns_table_with_ccg_level_integration_times_columns():
             "Requesting practice name": [requesting_practice_name],
             "Requesting practice ODS": [requesting_practice_ods_code],
             "GP2GP Transfers received": [1],
-            # "Integrated within 3 days": [None],
+            "Integrated within 3 days": [1],
+            # "Integrated within 3 days - %": [None],
+            # "Integrated within 8 days" : [None],
+            # "Integrated within 8 days - %": [None],
+            # "Not integrated within 8 days (integrated late + not integrated)": [None],
+            # "Not integrated within 8 days (integrated late + not integrated) - %": [None],
+            # "Integrated Late": [None],
+            # "Integrated Late - %": [None],
+            # "Not integrated within 14 days": [None],
+            # "Not integrated within 14 days - %": [None]
+        }
+    )
+
+    assert actual == expected
+
+
+@pytest.mark.filterwarnings("ignore:Conversion of")
+def test_returns_table_with_ccg_level_integration_times_columns_without_integrated_within_3_days():
+    requesting_practice_name = a_string(6)
+    requesting_practice_ods_code = a_string(6)
+    requesting_practice_ccg_ods_code = a_string(3)
+    requesting_practice_ccg_name = a_string(6)
+    conversation_id = a_string(16)
+    status = TransferStatus.TECHNICAL_FAILURE.value
+    failure_reason = TransferFailureReason.FINAL_ERROR.value
+    table = (
+        PaTableBuilder()
+        .with_row(
+            requesting_practice_name=requesting_practice_name,
+            requesting_practice_ods_code=requesting_practice_ods_code,
+            requesting_practice_ccg_ods_code=requesting_practice_ccg_ods_code,
+            requesting_practice_ccg_name=requesting_practice_ccg_name,
+            conversation_id=conversation_id,
+            status=status,
+            failure_reason=failure_reason,
+            sla_duration=timedelta(days=1).total_seconds(),
+        )
+        .build()
+    )
+
+    report_generator = CCGLevelIntegrationTimesReportsGenerator(table)
+    actual_table = report_generator.generate()
+    actual = actual_table.select(
+        [
+            "CCG name",
+            "CCG ODS",
+            "Requesting practice name",
+            "Requesting practice ODS",
+            "GP2GP Transfers received",
+            "Integrated within 3 days",
+            # "Integrated within 3 days - %",
+            # "Integrated within 8 days",
+            # "Integrated within 8 days - %",
+            # "Not integrated within 8 days (integrated late + not integrated)",
+            # "Not integrated within 8 days (integrated late + not integrated) - %",
+            # "Integrated Late",
+            # "Integrated Late - %",
+            # "Not integrated within 14 days",
+            # "Not integrated within 14 days - %"
+        ]
+    )
+
+    expected = pa.table(
+        {
+            "CCG name": [requesting_practice_ccg_name],
+            "CCG ODS": [requesting_practice_ccg_ods_code],
+            "Requesting practice name": [requesting_practice_name],
+            "Requesting practice ODS": [requesting_practice_ods_code],
+            "GP2GP Transfers received": [1],
+            "Integrated within 3 days": [0],
             # "Integrated within 3 days - %": [None],
             # "Integrated within 8 days" : [None],
             # "Integrated within 8 days - %": [None],
