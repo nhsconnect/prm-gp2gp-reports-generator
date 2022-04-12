35431c97c1e62eed2ff6f991f9320a6d7a2b946a
Ian Griffiths <ian.griffiths@thoughtworks.com>
2022-04-12 16:18:25 +0100

    PRMT-2611 [IG/JN] Improve transfer details by hour test

--- tests/unit/domain/test_generate_transfer_details_per_hour.py
+++ tests/unit/domain/test_generate_transfer_details_per_hour.py
@@ -10,7 +10,6 @@ from tests.builders.pa_table import PaTableBuilder
 
 
 def test_total_transfers_shown_per_hour():
-    # Given
     input_data = (
         PaTableBuilder()
         .with_row(
@@ -36,22 +35,40 @@ def test_total_transfers_shown_per_hour():
                 UTC
             ),
             status=TransferStatus.UNCLASSIFIED_FAILURE.value,
+        ).with_row(
+            date_requested=a_datetime(year=2021, month=1, day=1, hour=14, minute=10).astimezone(
+                UTC
+            ),
+            status=TransferStatus.PROCESS_FAILURE.value,
+        ).with_row(
+            date_requested=a_datetime(year=2021, month=1, day=2, hour=15, minute=45).astimezone(
+                UTC
+            ),
+            status=TransferStatus.UNCLASSIFIED_FAILURE.value,
+        ).with_row(
+            date_requested=a_datetime(year=2021, month=1, day=2, hour=14, minute=30).astimezone(
+                UTC
+            ),
+            status=TransferStatus.TECHNICAL_FAILURE.value,
+        ).with_row(
+            date_requested=a_datetime(year=2021, month=1, day=2, hour=14, minute=30).astimezone(
+                UTC
+            ),
+            status=TransferStatus.INTEGRATED_ON_TIME.value,
         )
         .build()
     )
 
     expected_output = pa.table(
         {
-            "Date/Time": ["21/01/01 12:00", "21/01/01 14:00", "21/01/02 15:00"],
-            "Total number of transfers": [2, 1, 1],
-            "Total technical failures": [2, 0, 0],
-            "Total unclassified failures": [0, 1, 1],
+            "Date/Time": ["21/01/01 12:00", "21/01/01 14:00", "21/01/02 14:00", "21/01/02 15:00"],
+            "Total number of transfers": [2, 2, 2, 2],
+            "Total technical failures": [2, 0, 1, 0],
+            "Total unclassified failures": [0, 1, 0, 2],
         }
     )
 
-    # When
     report_generator = TransferDetailsPerHourReportsGenerator(input_data)
     result = report_generator.generate()
 
-    # Then
     assert result == expected_output
